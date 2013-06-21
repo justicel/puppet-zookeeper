44f7382d5d828a64e668b79fbe6bbac7a749037b
Justice London <jlondon@syrussystems.com>
2013-06-21 15:47:22 -0400

    Updated server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -4,7 +4,7 @@ class zookeeper::server {
     command => 'zkServer.sh start',
     cwd     => "${zookeeper::params::zookeeper_home}/bin",
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin',
-                "${zookeeper::params::zookeeper_home/bin"
+                "${zookeeper::params::zookeeper_home}/bin"
                ],
     require => File[$zookeeper::params::zookeeper_home],
   }
