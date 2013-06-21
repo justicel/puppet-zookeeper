7456c26294ea9157f8293a410ddc18ffd5fd12f3
Justice London <jlondon@syrussystems.com>
2013-06-21 17:05:35 -0400

    Changed to add test for service.

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,12 +1,13 @@
 class zookeeper::server {
 
   exec { 'zookeeper-start':
-    command => 'zkServer.sh start',
+    command => 'zkServer.sh restart',
     cwd     => "${zookeeper::params::zookeeper_home}/bin",
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin',
                 "${zookeeper::params::zookeeper_home}/bin"
                ],
     require => File[$zookeeper::params::zookeeper_home],
+    unless  => "netstat -ln | grep ':3888'",
   }
 
 }
